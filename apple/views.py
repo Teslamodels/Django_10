c2497367686f486db046b7873577a6bcd87f447d
Azizbek <azizbekshomuratov2008@gmail.com>
2023-11-22 19:27:21 +0500

    Math

---
+++ apple/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+
+class Steve(TemplateView):
+    template_name = 'index.html'
+    
+class Jobs(TemplateView):
+    template_name = 'apple.html'
+    
